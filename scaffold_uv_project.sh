9c652e137b0cabd8ffb978fd9e353d54bdac7158
Juan Rodriguez Hortala <juanrh@pm.me>
2026-05-01 18:47:28 +0200

    Run scaffolding on a temp dir

--- scaffold_uv_project.sh
+++ scaffold_uv_project.sh
@@ -5,7 +5,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # See params on
 # https://github.com/osprey-oss/cookiecutter-uv/blob/main/cookiecutter.json
 
-pushd "${SCRIPT_DIR}/pdd-test"
+# Run cookiecutter in a temporary directory to avoid polluting pdd-test on failure
+TMP_DIR=$(mktemp -d)
+trap 'rm -rf "${TMP_DIR}"' EXIT
+
+pushd "${TMP_DIR}"
 
 uvx cookiecutter --no-input \
   https://github.com/osprey-oss/cookiecutter-uv \
@@ -22,13 +26,19 @@ uvx cookiecutter --no-input \
   type_checker="mypy" \
   open_source_license="Apache Software License 2.0"
 
+popd
+
+# Copy the generated project to pdd-test
+DEST_DIR="${SCRIPT_DIR}/pdd-test/calc-api"
+rm -rf "${DEST_DIR}"
+cp -R "${TMP_DIR}/calc-api" "${DEST_DIR}"
+
 # Prevent precommit messing up with the whole git repo
-sed -i '/@uv run pre-commit install/d' calc-api/Makefile
-sed -i 's/@uv run pre-commit run -a/@uv run pre-commit run --files ./g' calc-api/Makefile
-pushd calc-api
+sed -i '/@uv run pre-commit install/d' "${DEST_DIR}/Makefile"
+sed -i 's/@uv run pre-commit run -a/@uv run pre-commit run --files ./g' "${DEST_DIR}/Makefile"
+pushd "${DEST_DIR}"
 make install check
 make check test
-popd
 popd
 
 echo "Project scaffolding complete"
